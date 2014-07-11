ae69e219435dafdefa68287ef745c171cf00ef9a
emerak <stephaniacernas@gmail.com>
2014-07-11 01:03:28 -0500

    Validates presence of content

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 describe PostsController do
 
   it 'redirects to the homepage after saving' do
-    post :create, attributes_for(:post)
+    post :create, post: attributes_for(:post)
     expect(response).to redirect_to root_path
   end
 
 
   it 'saves the post successfully' do
     expect {
-      post :create, attributes_for(:post)
+      post :create, post: attributes_for(:post)
     }.to change(Post,:count).by(1)
   end
 
   it 'does not save the post' do
     expect {
-      post :create, attributes_for(:post)
-    }.to change(Post,:count).by(1)
+      post :create, post: attributes_for(:post, content: nil)
+    }.to change(Post,:count).by(0)
   end
 end
