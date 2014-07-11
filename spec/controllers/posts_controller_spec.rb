e5210225a2b2f2c9b0feade7bafe33a9ffe2de05
emerak <stephaniacernas@gmail.com>
2014-07-11 00:44:15 -0500

    Tests save post

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe PostsController do
+
+  it 'redirects to the homepage after saving' do
+    post :create, attributes_for(:post)
+    expect(response).to redirect_to root_path
+  end
+
+
+  it 'saves the post successfully' do
+    expect {
+      post :create, attributes_for(:post)
+    }.to change(Post,:count).by(1)
+  end
+
+end
