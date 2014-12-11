3599fa798a2bcc74d262953934eea85b42566416
emerak <stephaniacernas@gmail.com>
2014-12-11 12:44:15 -0600

    Test cis

---
+++ features/step_definitions/global_steps.rb
@@ -0,0 +1,14 @@
+Given(/^I am an signed user$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am in the log in page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I enter valid "(.*?)" and "(.*?)" information$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the index page$/) do
+  pe
